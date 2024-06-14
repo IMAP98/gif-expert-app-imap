b4b1b38ea9dc28ea1ca51c89df09f08e80f86206
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-06-14 14:16:40 -0600

    Hooks added

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,31 @@
-import { getGifs } from "../helpers/getGifs";
-
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
 
-    getGifs( category );
+    const { images, isLoading } = useFetchGifs( category );
 
     return (
         <>
             <h3>{ category }</h3>
+
+            {
+                isLoading && ( <h2>Cagando...</h2> )
+            }
+
+            <div className="card-grid">
+                    {
+                        images.map( 
+                            (image) => (
+                                <GifItem 
+                                    key={ image.id }
+                                    { ...image }
+                                />
+                            )
+                        )
+                    }
+            </div>
         </>
     )
 }
+
